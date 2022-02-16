92dcd98e0a4226eb17510704e50365cd7d95ca80
utsman <muhammad.utsman@ruangguru.com>
2022-02-16 09:02:57 +0700

    Add versioning for jitpack

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -15,7 +15,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.utsmannn"
             artifactId = "hana-doc"
-            version = "0.0.6"
+            version = "0.0.7"
 
             from(components["java"])
         }
@@ -23,7 +23,7 @@ publishing {
 }
 
 group = "me.hana"
-version = "0.0.6"
+version = "0.0.7"
 
 dependencies {
 
